1b6a3bc41b6e33338386310cc1955d2efe8ffc11
orisa <orisa1234tolu@gmail.com>
2025-08-08 11:13:03 +0100

    worked on the test file to create a log file for monitoring the pipeline

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,5 +1,31 @@
 # tests/test_job.py
-from etl.job import transform
+import sys
+import os
+import logging
+
+logging.basicConfig(filename="C:\\Users\\orisa\\Documents\\myproject\\testresult.log",
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+#etl = "C:\\Users\\orisa\\Documents\\myproject\\etl"
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(os.path.join(parent_dir, "etl"))
+
+#if etl not in sys.path:
+#    sys.path.append(etl)
+
+
+from job import *
 
 def test_transform():
-    assert transform([1, 2]) == [2, 4]
+    if transform([1, 2]) == [2, 4]:
+        logging.info("Test Passed")
+    else:
+        logging.error("Test Failed")
+
+if __name__ == "__main__":
+    test_transform() 
+    
+    #logging.INFO(assert transform([1, 2]) == [2, 4])
